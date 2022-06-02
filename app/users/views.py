1067dee147d1eaf8cd8907c351e391f9b0b76da4
Nunomatos7 <nuno.esteves@ua.pt>
2022-06-02 14:16:28 +0100

    Inicio Personal Dashboard

--- app/users/views.py
+++ app/users/views.py
@@ -39,6 +39,6 @@ def personal_dashboard(request):
                     tasks.append(task)
 
     return render(request, 'users/personal_dashboard.html', {'title': f"{user_prof.projects.first().title}'s dashboard",
-                                                             'projects': user_prof.projects.all().values(), 'tasks': tasks})
+                                                             'projects': user_prof.projects.all().values()}) #, 'tasks': tasks
 
 
